1e166faf59a8f4fa4b6951bde4bda09f8477ec74
shashjha <shashankshankar77@gmail.com>
2020-04-05 00:48:12 +0530

    test

--- app/src/main/java/com/example/artisphere/Adapter/PostAdapter.java
+++ app/src/main/java/com/example/artisphere/Adapter/PostAdapter.java
@@ -1,4 +1,74 @@
 package com.example.artisphere.Adapter;
 
-public class PostAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.artisphere.Model.Post;
+import com.example.artisphere.R;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    public Context mContext;
+    public List<Post> mPost;
+
+    private FirebaseUser firebaseUser;
+
+
+    public PostAdapter(Context mContext, List<Post> mPost) {
+        this.mContext = mContext;
+        this.mPost = mPost;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.post_item, viewGroup,false);
+        return new PostAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPost.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView image_profile, post_image, clap, comment, save;
+        public TextView username, claps, comments , description, read_comments;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            image_profile = itemView.findViewById(R.id.image_profile);
+            post_image = itemView.findViewById(R.id.post_image);
+            clap = itemView.findViewById(R.id.clap);
+            comment = itemView.findViewById(R.id.comment);
+            save = itemView.findViewById(R.id.save);
+            username = itemView.findViewById(R.id.username);
+            claps = itemView.findViewById(R.id.claps);
+            comments = itemView.findViewById(R.id.comments);
+            description = itemView.findViewById(R.id.description);
+            read_comments = itemView.findViewById(R.id.read_comments);
+
+
+
+
+        }
+    }
 }

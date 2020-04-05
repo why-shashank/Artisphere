b92db7d8533e2ae9e4d4f36579bd8c9289f348d0
shashjha <shashankshankar77@gmail.com>
2020-04-05 20:50:42 +0530

    posting and liking complete

--- app/src/main/java/com/example/artisphere/Adapter/PostAdapter.java
+++ app/src/main/java/com/example/artisphere/Adapter/PostAdapter.java
@@ -10,10 +10,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.artisphere.Model.Post;
+import com.example.artisphere.Model.User;
 import com.example.artisphere.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -38,8 +45,38 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        final Post post = mPost.get(position);
+
+        Glide.with(mContext).load(post.getPostImage()).into(holder.post_image);
+
+        if (post.getDescription().equals("")){
+            holder.description.setVisibility(View.GONE);
+        }
+        else {
+            holder.description.setVisibility(View.VISIBLE);
+            holder.description.setText(post.getDescription());
+        }
+
+        publisherInfo(holder.image_profile,holder.username,holder.fullname,post.getPublisher());
+        isClapped(post.getPostid(),holder.clap);
+        nrClaps(holder.claps,post.getPostid());
+
+        holder.clap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.clap.getTag().equals("Clap")){
+                    FirebaseDatabase.getInstance().getReference().child("Claps").child(post.getPostid())
+                            .child(firebaseUser.getUid()).setValue(true);
+                }
+                else {
+                    FirebaseDatabase.getInstance().getReference().child("Claps").child(post.getPostid())
+                            .child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
     }
 
     @Override
@@ -50,7 +87,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         public ImageView image_profile, post_image, clap, comment, save;
-        public TextView username, claps, comments , description, read_comments;
+        public TextView username,fullname, claps, comments , description, read_comments;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
@@ -61,6 +98,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             comment = itemView.findViewById(R.id.comment);
             save = itemView.findViewById(R.id.save);
             username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
             claps = itemView.findViewById(R.id.claps);
             comments = itemView.findViewById(R.id.comments);
             description = itemView.findViewById(R.id.description);
@@ -70,5 +108,67 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
 
 
         }
+    }
+
+    private void isClapped(String postid, final ImageView imageView){
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Claps")
+                .child("postid");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(firebaseUser.getUid()).exists()){
+                    imageView.setImageResource(R.drawable.ic_clapped);
+                    imageView.setTag("Clapped");
+                }
+                else {
+                    imageView.setImageResource(R.drawable.ic_claps);
+                    imageView.setTag(("Clap"));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void nrClaps(final TextView claps, String postid){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Claps")
+                .child("postid");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                claps.setText(dataSnapshot.getChildrenCount()+" Claps");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    private void publisherInfo(final ImageView image_profile, final TextView username, final TextView fullname, String userid ){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Glide.with(mContext).load(user.getDp()).into(image_profile);
+                username.setText(user.getUsername());
+                fullname.setText(user.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
